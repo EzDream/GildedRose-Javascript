298f023aa24c3eb1f3cab0efdcfadf1c23b13d86
Piny <ezdream@icloud.com>
2023-11-29 18:15:02 +1300

    refactor: wrap if logic

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -24,16 +24,16 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name !== Item.Type.AgedBrie && item.name !==  Item.Type.Backstage) {
+      if (this.notAgedBrie(item) && this.notBackstage(item)) {
         if (item.quality > 0) {
-          if (item.name !== Item.Type.Sulfuras) {
+          if (this.notSulfuras(item)) {
             item.quality = item.quality - 1;
           }
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-          if (item.name === Item.Type.Backstage) {
+          if (this.isBackstage(item)) {
             if (item.sellIn < 11) {
               if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -47,14 +47,14 @@ class Shop {
           }
         }
       }
-      if (item.name !== Item.Type.Sulfuras) {
+      if (this.notSulfuras(item)) {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name !== Item.Type.AgedBrie) {
-          if (item.name !== Item.Type.Backstage) {
+        if (this.notAgedBrie(item)) {
+          if (this.notBackstage(item)) {
             if (item.quality > 0) {
-              if (item.name !== Item.Type.Sulfuras) {
+              if (this.notSulfuras(item)) {
                 item.quality = item.quality - 1;
               }
             }
@@ -70,6 +70,22 @@ class Shop {
     })
 
     return this.items;
+  }
+
+  notSulfuras(item) {
+    return item.name !== Item.Type.Sulfuras
+  }
+
+  isBackstage(item) {
+    return item.name === Item.Type.Backstage
+  }
+
+  notBackstage(item) {
+    return item.name !== Item.Type.Backstage
+  }
+
+  notAgedBrie(item) {
+    return item.name !== Item.Type.AgedBrie
   }
 }
 module.exports = {
