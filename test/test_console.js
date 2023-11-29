456cb6ddcdb2e5341483ac430306eb8c20225881
Piny <ezdream@icloud.com>
2023-11-29 17:31:29 +1300

    chore: add test console

--- test/test_console.js
+++ test/test_console.js
@@ -1,3 +1,24 @@
 /**
  * Created by zuoxiansheng on 29/11/23
  */
+
+let logs = []
+
+function clearLogs() {
+  logs = []
+}
+
+function log(info) {
+  logs.push(info)
+  console.log(info)
+}
+
+function getAllLogs() {
+  logs.join('\n')
+}
+
+module.exports = {
+  clearLogs,
+  log,
+  getAllLogs,
+}
